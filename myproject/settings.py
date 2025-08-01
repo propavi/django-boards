1220582f929619541fa8a0cdb064292ae0c26b2c
propavi <pavithrannatraj07@gmail.com>
2025-08-01 13:43:12 +0530

    Made changes in html file and added css to it

--- myproject/settings.py
+++ myproject/settings.py
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'myproject.urls'
 
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from boards.views import home  
 
 urlpatterns = [
+    path("", home, name="home"),  
     path('admin/', admin.site.urls),
 ]
+
